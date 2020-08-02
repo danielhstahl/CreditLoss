e64481dace70218fd4f3d6c854b4329188e93b29
Daniel Stahl <danstahl1138@gmail.com>
2020-08-02 17:06:36 -0500

    failing portfolio test for now

--- src/test/scala/com/danielhstahl/creditloss/LoanLoss.scala
+++ src/test/scala/com/danielhstahl/creditloss/LoanLoss.scala
@@ -23,4 +23,27 @@ class LoanLossDistribution extends FunSuite with DataFrameSuiteBase {
     assert(portfolioMetrics.el.length === 3)
     assert(portfolioMetrics.variance.length === 3)
   }
+  test("gets correct expectation"){
+    val sqlCtx = sqlContext
+    import sqlCtx.implicits._
+    val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
+    val loanDF =
+      LoanLossSim.simulatePortfolio(spark, 30, 42).withColumn("num", lit(1))
+    val portfolioMetrics =
+      LoanLoss.getPortfolioMetrics(
+        loanDF,
+        256,
+        -30 * 1000000.0,
+        (u: Complex, l: Double, lgdVariance: Double) => (u * l).exp,
+        LoanLoss.getLiquidityRiskFn(50.0, 0.0)
+      )
+    val mgfcf=(v: Seq[Complex])=>v.sum.exp
+    val cf=LoanLoss.getFullCF(3, portfolioMetrics.cf, mgfcf)
+    val el=portfolioMetrics.el.sum
+    val distEl=FangOost.getExpectation(-30 * 1000000.0, 0.0, cf)
+    print(el)
+    print(distEl)
+    assert(math.abs(el-distEl)<1000)
+
+  }
 }
